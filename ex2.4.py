d8fe3da77e5f07f897d7f2fc540fd6b3b4e3ad0a
meetuofcbhatt <meet.bhatt@ucalgary.ca>
2023-02-12 17:22:02 -0700

    json file added.

--- ex2.4.py
+++ ex2.4.py
@@ -3,13 +3,13 @@ import json
 import timeit
 import matplotlib .pyplot as plt
 
-def quick_sort(arr, low, high):
+def func1(arr, low, high):
     if low < high:
-        pi = partition(arr, low, high)
-        quick_sort(arr, low, pi - 1)
-        quick_sort(arr, pi + 1, high)
+        pi = func2(arr, low, high)
+        func1(arr, low, pi - 1)
+        func1(arr, pi + 1, high)
 
-def partition(array, start, end):
+def func2(array, start, end):
     pivot = random.choice(array[start:end + 1])
     low = start
     high = end
@@ -24,24 +24,6 @@ def partition(array, start, end):
             high -= 1
     return low
 
-def insertion_sort(array, start, end):
-    for i in range(start + 1, end + 1):
-        key = array[i]
-        j = i - 1
-        while j >= start and array[j] > key:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
-
-def sort(array):
-    n = len(array)
-    if n < 10:
-        insertion_sort(array, 0, n - 1)
-    else:
-        quick_sort(array, 0, n - 1)
-    return array
-
-
 def main():
     with open("ex2.json", "r") as inF:
         content = json.load(inF)
@@ -50,7 +32,8 @@ def main():
     array_length = []
 
     for array in content:
-        time_req = timeit.timeit(lambda: sort(array), number = 1)
+        l = len(array)
+        time_req = timeit.timeit(lambda: func1(array, 0, l - 1), number = 1)
         time_complexity.append(time_req)
         array_length.append(len(array))
     
